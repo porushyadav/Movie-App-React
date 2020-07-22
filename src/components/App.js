c2cdfa581d5cfa27f79a78d9c346bc55c6e354d9
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-07-22 22:40:37 +0530

    added favourites and tab navs

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-import {addMovies} from '../actions';
+import {addMovies, setShowFavourites} from '../actions';
 class App extends React.Component {
 
   componentDidMount()
@@ -19,25 +19,52 @@ class App extends React.Component {
 
   }
 
+  isMovieFavourite=(movie)=>{
+
+    const{favourites}=this.props.store.getState();
+
+    const index=favourites.indexOf(movie);
+    if(index!==-1)
+    {
+      return true;
+    }
+
+    return false;
+
+  }
+
+  onChangeTab=(val)=>{
+
+    this.props.store.dispatch(setShowFavourites(val));
+
+  }
+
   render()
   {
-    const {list} =this.props.store.getState();
+    const {list,favourites,showFavourites} =this.props.store.getState();
     console.log('Render',this.props.store.getState());
+
+    const displayMovies=showFavourites?favourites:list;
 
   return (
     <div className="App">
     <Navbar />
     <div className="main">
     <div className="tabs">
-      <div className="tab">Movies</div>
-      <div className="tab">Favourites</div>
+      <div className={`tab ${showFavourites? '' : 'active-tabs'} `} onClick={()=>this.onChangeTab(false)}>Movies</div>
+      <div className={`tab ${showFavourites? 'active-tabs' : '' }`} onClick={()=>this.onChangeTab(true)}>Favourites</div>
     </div>
     <div className="list">
     {
-      list.map((movie,index)=>(
-        <MovieCard movie={movie} key={`movies-${index}`}/>
+      displayMovies.map((movie,index)=>(
+        <MovieCard 
+        movie={movie} 
+        key={`movies-${index}`} 
+        dispatch={this.props.store.dispatch}
+        isFavourite={this.isMovieFavourite(movie)}/>
       ))}
     </div>
+    {displayMovies.length===0?<div className="no-movies">no movies to display</div>:null}
     </div>
       
     </div>
