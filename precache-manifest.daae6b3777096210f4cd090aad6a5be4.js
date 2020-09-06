8ee9a9e6766484833638ac4c0a3109ba3ea63e7b
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-09-06 12:43:16 +0530

    Updates

---
+++ precache-manifest.daae6b3777096210f4cd090aad6a5be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586f68a64fe5ad6f23e73e6d61f6e7d4",
+    "url": "/Movie-App-React/index.html"
+  },
+  {
+    "revision": "e00048773a7029da603f",
+    "url": "/Movie-App-React/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "22a4da19e7e2ea44ed7e",
+    "url": "/Movie-App-React/static/js/2.0e963f9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-App-React/static/js/2.0e963f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00048773a7029da603f",
+    "url": "/Movie-App-React/static/js/main.d065e6f4.chunk.js"
+  },
+  {
+    "revision": "bb6f21675def112aaf12",
+    "url": "/Movie-App-React/static/js/runtime-main.f38ddadd.js"
+  }
+]);
